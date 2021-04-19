829d17d646b50f11385f11dc554b6c7daa294c62
Jorge Ernesto Gomez Giraldo <ernesto2066@gmail.com>
2021-04-19 15:23:33 -0500

    add composer .json faker library

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (env('REDIRECT_HTTPS')) 
+        {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -22,9 +27,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
         Schema::defaultStringLength(191);
+        //Metodo para la redireccion de las URL de laravel en el hosting
+        if (env('REDIRECT_HTTPS')) 
+        {
+            $url->formatScheme('https://');
+        }
     }
 }
